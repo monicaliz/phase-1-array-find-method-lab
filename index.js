36a45ec632771332ae0761b4045e069d31f34395
monicaliz <monicaeliz3@gmail.com>
2022-06-05 17:31:42 -0400

    commit tree 1d2076cda836535eee8b70391697a2ba4b517bee

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+
+ superbowlWin = (record) => {
+ const result = record.find( record => 
+     record.result === "W" )
+     return !!result ? result.year : undefined;
+ }
+ 
